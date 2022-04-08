d7af732f9532306823a1d484283a38daa98ef318
AlexheroUK <Olmine2013@gmail.com>
2022-04-08 18:51:43 +0300

    Major fix Made it all work, full JWT authorization is available
    Signed-off-by: AlexheroUK <Olmine2013@gmail.com>

--- users/views.py
+++ users/views.py
@@ -4,6 +4,11 @@ from users.serializers import LoginSerializer, \
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.permissions import AllowAny
+from rest_framework.exceptions import AuthenticationFailed
+from users.models import User
+import jwt
+from datetime import datetime
+from datetime import timedelta
 
 
 class RegistrationAPIView(APIView):
@@ -27,11 +32,28 @@ class LoginAPIView(APIView):
 	serializer_class = LoginSerializer
 
 	def post(self, request):
-		serializer = self.serializer_class(data=request.data)
+		email = request.data["email"]
+		password = request.data["password"]
 
-		if serializer.is_valid():
-			return Response(serializer.data, 
-				status=status.HTTP_200_OK)
+		user = User.objects.filter(email=email).first()
 
-		return Response(serializer.errors, 
-			status=status.HTTP_400_BAD_REQUEST)	
+		if user is None:
+			raise AuthenticationFailed("User doesn't exist")
+
+		if not user.check_password(password):
+			raise AuthenticationFailed("Wrong password")
+
+		import os
+		from dotenv import find_dotenv, load_dotenv
+		dt = datetime.now() + timedelta(days=30)
+
+		load_dotenv(find_dotenv('./.env'))
+
+		token = jwt.encode({
+			'email': user.email,
+			'exp': int(dt.timestamp())
+		}, key=os.getenv("JWT_CODE"), algorithm='HS256')
+
+		return Response({
+			"token": token
+		})
