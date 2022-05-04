4f632a6d8fb731cc75505766306bee5131b2a834
Oleksii Datsiuk <Olmine2013@gmail.com>
2022-05-04 10:49:46 +0300

    Some changes

--- secretshop/settings.py
+++ secretshop/settings.py
@@ -59,7 +59,9 @@ MIDDLEWARE = [
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware'
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'secretshop.urls'
